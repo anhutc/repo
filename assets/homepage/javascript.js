d1f59c3dbcc710c5ceb5c53fda5c9cb91223c696
anhutc <anhutck58@gmail.com>
2022-06-20 18:38:41 +0700

    dva

--- assets/homepage/javascript.js
+++ assets/homepage/javascript.js
@@ -1,5 +1,5 @@
 // Decode Packages File into Object Array
-packages = decodePackagesFile(loadXMLDoc("../../Packages"));
+packages = decodePackagesFile(loadXMLDoc("https://anhutc.github.io/repo/Packages"));
 // Render Packages File
 for (i = 0; i < packages.length; i++) {
   var color = "";
