b5c493df8132014c1a4f7e403c3bb15f8d5a8b40
anhutc <anhutck58@gmail.com>
2022-06-24 18:21:51 +0700

    Update javascript.js

--- assets/homepage/javascript.js
+++ assets/homepage/javascript.js
@@ -178,3 +178,18 @@ function setWidth(classID) {
     "s linear infinite";
   calculateAnimationWidthOf(classID);
 }
+
+//Duplicating Function
+function duplicateContentOf(classID) {
+  document.getElementById(classID).innerHTML =
+    document.getElementById(classID).innerHTML +
+    document.getElementById(classID).innerHTML +
+    document.getElementById(classID).innerHTML +
+    document.getElementById(classID).innerHTML;
+  setWidth(classID);
+}
+
+//Duplicate and fit width of all scrollers
+duplicateContentOf("Apps");
+duplicateContentOf("Hacks");
+duplicateContentOf("Others");
