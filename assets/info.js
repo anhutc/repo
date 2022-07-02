587f23bf85a6ad40b8bec5f19ac88576fe8b7c7a
anhutc <anhutck58@gmail.com>
2022-07-02 19:14:38 +0700

    a

--- assets/info.js
+++ assets/info.js
@@ -78,16 +78,6 @@ function addScreenshot(n) {
     }
 }
 
-$(".screenshots img").click(function () {
-    var $src = $(this).attr("src");
-    $(".show").fadeIn();
-    $(".img-show img").attr("src", $src)
-})
-
-$("span, .overlay").click(function () {
-    $(".show").fadeOut()
-})
-
 
 //Load initital wrapper
     addScreenshot(1)
